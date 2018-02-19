346b490466bb4161a4452fc9e0a1f522fb5598be
BETH-A <elizabeth78@me.com>
2018-02-18 21:34:07 -0500

    Hangman-Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,36 @@
-var words = ["snow leopard", "cheetah", "jaguars", "panther", "bengal tiger", "asiatic lion"];
+var word = ["snow leopard", "cheetah", "jaguars", "panther", "bengal tiger", "asiatic lion"];
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+    'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+];
+var userGuess = [];
+var wordToDisplay = "";
+
+var wtgID = document.getElementById(word);
 
 console.log("hi");
-document.onkeyup = function(event) {
+    var userGuess = word.key;
+
+    for (var i = 0; i < word.length; i++) {
+document.onkeyup = function (event) {
     console.log(event.key)
-    var userGuess = event.key;
+        var currentLetter = word.charAt(i);
+        // Check the letters
+        // If matches in the word, add to wordToDisplay
+        if (userGuess.indexOf(currentLetter) > -1) {
+            console.log("Match");
+            wordToDisplay = wordToDisplay + currentLetter + " ";
+        } else {
+            // No match, add '_' to wordToDisplay
+            wordToDisplay = wordToDisplay + "_";
+        }
+        console.log(wordToDisplay)
+    }
+
+    updateDisplay(wordToDisplay);
+
+    function updateDisplay(newWord) {
+        wtgId.innerText = newWord;
+    }
+
 }
---
+++ assets/javascript/game3.js
@@ -0,0 +1,43 @@
+//VARIABLES
+
+//Object = try to guess the letters that make up each word
+var word = ["snow leopard", "cheetah", "jaguars", "panther", "bengal tiger", "asiatic lion"];
+
+//Letters guessed
+var guesses = [];
+
+//# of guesses left to be subtracted by 1 each time a wrong letter is guessed
+var guessesLeft = 7;
+
+//Possible letters to guess
+var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+
+
+
+//FUNCTIONS
+
+//Pick random word from word array
+    function randomWordToGuess() {
+
+    }
+
+//Show '_' for each letter to be represented in the word
+
+//Compare letter picked to word to be guessed
+    //If the letter is in the word, display letter
+
+    //If the letter is not in the word, display in bad guesses array
+
+//Reduce # of guesses by 1 for each bad guess
+    //Only reduce # of guesses once, so if user guesses the same letter it does not cost them
+
+    function updateGuessesLeft() {
+        document.querySelector("guessesLeft").textContent = "Guesses Left: " guessesLeft[i]-- ;
+    }
+
+//When full word is guess, display "YOU WIN"
+
+// If the word is not guessed within 7 guesses, display "YOU LOOSE"
