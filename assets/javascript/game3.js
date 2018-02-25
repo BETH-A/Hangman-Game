21066083c514e63610420a658bb25643e16f8d9a
BETH-A <elizabeth78@me.com>
2018-02-24 22:04:57 -0500

    Hangman-Game

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -3,7 +3,6 @@
 //Object = try to guess the letters that make up each word
 var word = ["leopard", "cheetah", "jaguars", "panther", "tiger", "lion"];
 
-
 //Possible letters to guess
 var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
     'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
@@ -19,7 +18,6 @@ var randomWordLength = randomWord.length;
 var winCounter = 0;
 // var wins = 0;
 // var loss = 0;
-
 
 //FUNCTIONS
 
@@ -59,48 +57,37 @@ function winLose() {
     } else if (guessesRemaining === 0) {
         document.getElementById("guessesRemaining").textContent = guessesRemaining;
         alert("I can't believe you couldn't guess the word, " + randomWord + "! You lose.");
-        //  startGame();
-
-
     }
 }
-// document.onkeyup = function (event) {
-
-// }
 //If the letter is not in the word, display in bad guesses array
 
 document.onkeyup = function (event) {
-        userGuesses = event.key;
+    userGuesses = event.key;
 
+    if (randomWord.indexOf(userGuesses) > -1) {
+        for (var i = 0; i < randomWord.length; i++) {
 
+            if (randomWord[i] === userGuesses) {
 
-        if (randomWord.indexOf(userGuesses) > -1) {
-            for (var i = 0; i < randomWord.length; i++) {
-
-
-                if (randomWord[i] === userGuesses) {
-
-                    dashes[i] = userGuesses;
-                    document.getElementById("blanks").textContent = dashes.join(" ");
-                    // console.log(dashes);
-                    winCounter++;
-                    winLose();
-                }
-            }
-        } else {
-            if (badGuess.indexOf(userGuesses) === -1) {
-                badGuess.push(userGuesses);
-
-                document.getElementById("wrong-guesses").textContent = badGuess
-
-
-                guessesRemaining = guessesRemaining - 1;
-                document.getElementById("guessesRemaining").textContent = guessesRemaining;
-                console.log(badGuess);
+                dashes[i] = userGuesses;
+                document.getElementById("blanks").textContent = dashes.join(" ");
+                // console.log(dashes);
+                winCounter++;
                 winLose();
             }
         }
-    }   
+    } else {
+        if (badGuess.indexOf(userGuesses) === -1) {
+            badGuess.push(userGuesses);
 
-        startGame();
+            document.getElementById("wrong-guesses").textContent = badGuess
 
+            guessesRemaining = guessesRemaining - 1;
+            document.getElementById("guessesRemaining").textContent = guessesRemaining;
+            console.log(badGuess);
+            winLose();
+        }
+    }
+}
+
+startGame();
